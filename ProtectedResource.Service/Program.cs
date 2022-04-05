4ee88bb17a182652f65d03f2476e16a853623310
ArchimedesJr\Eli <dyslexicanaboko@gmail.com>
2022-04-05 01:34:17 -0400

    Added in boiler plate windows service, copied from dot net core examples.

---
+++ ProtectedResource.Service/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ProtectedResource.Service
+{
+    //https://github.com/dotnet/AspNetCore.Docs/tree/main/aspnetcore/host-and-deploy/windows-service/samples/3.x/BackgroundWorkerServiceSample
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var hostBuilder = CreateHostBuilder(args);
+
+            var host = hostBuilder.Build();
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
